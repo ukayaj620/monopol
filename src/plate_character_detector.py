ace64cd2ba6c0a54c317300ce6141de7bc2534f3
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2021-12-28 04:43:16 +0700

    feat: detect plate character (roi)

--- src/plate_character_detector.py
+++ src/plate_character_detector.py
@@ -3,24 +3,69 @@ import cv2
 
 class PlateCharacterDetector:
 
-  def __init__(self):
-    self.image_bgr = None
-    self.image_gray = None
-  
-  def load_image(self, file_path):
-    self.image_bgr = cv2.resize(cv2.imread(file_path), (240, 80))
-    self.image_gray = cv2.cvtColor(self.image_bgr, cv2.COLOR_BGR2GRAY)
-  
-  def create_kernel(self, size):
-    return cv2.getStructuringElement(cv2.MORPH_RECT, (size, size))
+    def __init__(self):
+        self.image_bgr = None
+        self.image_gray = None
+        self.digit_width = 32
+        self.digit_height = 64
 
-  def apply_gaussian_blur(self, image_gray, kernel_size):
-    return cv2.GaussianBlur(image_gray, kernel_size, 0)
+    def load_image(self, file_path):
+        self.image_bgr = cv2.resize(cv2.imread(file_path), (240, 80))
+        self.image_gray = cv2.cvtColor(self.image_bgr, cv2.COLOR_BGR2GRAY)
 
-  def apply_thresholding(self, image_gray):
-    return cv2.threshold(image_gray, 180, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    def detect_characters(self):
+        character_rois = self.image_bgr.copy()
+        crop_characters = []
+        image_bw, image_erode = self._preprocessing()
+        contours, _ = cv2.findContours(
+            image_erode, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-  def apply_erosion(self, image_bw, kernel):
-    return cv2.morphologyEx(image_bw, cv2.MORPH_ERODE, kernel)
+        for contour in self.sort_contours(contours=contours):
+            (x, y, w, h) = cv2.boundingRect(contour)
+            char_size_ratio = h / w
+            if 1.1 <= char_size_ratio <= 6:
+                char_height_ratio = h / self.image_bgr.shape[0]
+                if char_height_ratio >= 0.35:
+                    cv2.rectangle(character_rois, (x, y),
+                                  (x + w, y + h), (0, 255, 0), 2)
 
-  
+                    char = image_bw[y:(y + h), x:(x + w)]
+                    width_padding = int(char_size_ratio * w * 0.1)
+                    char = cv2.copyMakeBorder(
+                        char, 2, 2, width_padding, width_padding, cv2.BORDER_CONSTANT, (0, 0, 0))
+                    char = cv2.resize(char, dsize=(
+                        self.digit_width, self.digit_height))
+                    _, char = cv2.threshold(
+                        char, 220, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+                    crop_characters.append(char)
+
+        return character_rois, crop_characters
+
+    def sort_contours(self, contours, reverse=False):
+        i = 0
+        bounding_boxes = [cv2.boundingRect(contour) for contour in contours]
+        (contours, bounding_boxes) = zip(*sorted(zip(contours, bounding_boxes),
+                                                 key=lambda b: b[1][i], reverse=reverse))
+        return contours
+
+    def _preprocessing(self):
+        image_blur = self.apply_gaussian_blur(
+            image_gray=self.image_gray, kernel_size=(7, 7))
+        image_bw = self.apply_thresholding(image_gray=image_blur)
+        image_erode = self.apply_erosion(
+            image_bw=image_bw, kernel=self.create_kernel(3))
+
+        return image_bw, image_erode
+
+    def create_kernel(self, size):
+        return cv2.getStructuringElement(cv2.MORPH_RECT, (size, size))
+
+    def apply_gaussian_blur(self, image_gray, kernel_size):
+        return cv2.GaussianBlur(image_gray, kernel_size, 0)
+
+    def apply_thresholding(self, image_gray):
+        return cv2.threshold(image_gray, 180, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+    def apply_erosion(self, image_bw, kernel):
+        return cv2.morphologyEx(image_bw, cv2.MORPH_ERODE, kernel)
--- tests/test_plate_character_detector.py
+++ tests/test_plate_character_detector.py
@@ -7,13 +7,27 @@ from src.utils import show_plots, create_image_dict
 
 class TestPlateCharacterDetector(unittest.TestCase):
 
-  def setUp(self):
-    self.plate_character_detector = PlateCharacterDetector()
+    def setUp(self):
+        self.plate_character_detector = PlateCharacterDetector()
 
-  def test_load_image(self):
-    file_path = "./data/example/indonesia_nopol_1.JPG"
-    self.plate_character_detector.load_image(file_path)
-    image_bgr = self.plate_character_detector.image_bgr
-    image_gray = self.plate_character_detector.image_gray
-    images = [create_image_dict(image_bgr, "Color", None), create_image_dict(image_gray, "Gray", "gray")]
-    show_plots((6, 2), ncols=2, nrows=1, images=images)
+    def test_load_image(self):
+        file_path = "./data/example/indonesia_nopol_1.JPG"
+        self.plate_character_detector.load_image(file_path)
+        image_bgr = self.plate_character_detector.image_bgr
+        image_gray = self.plate_character_detector.image_gray
+        images = [create_image_dict(image_bgr, "Color", None), create_image_dict(
+            image_gray, "Gray", "gray")]
+        show_plots((6, 2), ncols=2, nrows=1, images=images)
+
+    def test_detect_character(self):
+        file_path = "./data/example/indonesia_nopol_5.JPG"
+        self.plate_character_detector.load_image(file_path)
+        (character_rois, crop_characters) = self.plate_character_detector.detect_characters()
+        plt.figure(figsize=(5, 3))
+        plt.axis(False)
+        plt.imshow(character_rois)
+
+        images = [create_image_dict(char, "", cmap="gray")
+                  for char in crop_characters]
+        show_plots((len(crop_characters), 2), ncols=len(
+            crop_characters), nrows=1, images=images)
